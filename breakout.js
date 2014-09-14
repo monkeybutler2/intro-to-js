546cb83d209d8f58577db53041c19ea20e1d51f0
chriscauley <chris@lablackey.com>
2014-09-14 06:37:50 -0400

    adding createBricks

--- breakout.js
+++ breakout.js
@@ -1,7 +1,43 @@
+
+function circle(x,y,r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function rect(x,y,w,h) {
+  ctx.beginPath();
+  ctx.rect(x,y,w,h);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function createBricks(o) {
+  var out = [], x, y, col_max, color;
+  var colors = ["red","green","blue","yellow"]
+  for (var row=0; row<4; row++) {
+    col_max = Math.floor(o.canvas.width / (o.w+o.s));
+    if (row%2 == 0) { col_max -=1; }
+    y = row*(o.h + o.s) + o.s;
+    for (var col=0; col<col_max; col++) {
+      x = col*(o.s + o.w) + o.s;
+      color = colors[Math.floor((row+col)/colors.length)];
+      out.push({x: x, y: y, color: color});
+    }
+  }
+}
+
 function startGame() {
   alert("Hello World"); //step 1
 
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  
+
+  var bricks = createBricks({w: 40, h: 15, s: 5, canvas: canvas})
+  console.log(bricks);
 }
